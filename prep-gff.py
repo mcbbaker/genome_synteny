95c345872af36b5f7952e73ccefc3fee174518a0
Mari Baker <118228251+mcbbaker@users.noreply.github.com>
2024-05-21 18:00:15 -0600

    Create prep-gff.py

---
+++ prep-gff.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# purpose: prep file that contains gff3 info (grepped for 'gene') to put in the format to run mcscanx
+# usage: python3 prep-gff.py gff-file sp-name (ie 'lc', NOT with a number)
+
+import sys
+import re
+
+gff = open(sys.argv[1],'r')
+sp = sys.argv[2]
+
+for line in gff:
+    line = line.strip('\n')
+    line = line.split('\t')
+    chrom = re.search("^.+\.Chr(\d)$",line[0])
+    # only keep Chrom entries
+    if chrom:
+        name = re.search("^ID=(.+);Name=",line[8])
+        print(sp + chrom.group(1), name.group(1), line[3], line[4], sep='\t')
+    # don't print out entries from unitigs
+    else:
+        pass
+
+gff.close()
